dab5eb130ea63f6fd9ea9be4ba688b49c3308a92
Maii <liebeee.maii@gmail.com>
2025-05-17 16:50:52 +0700

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
 import os
 
-BOT_TOKEN = os.environ["7663978407:AAHy49TWkU4BM1Y89MBXvEhQ8m9uN4aPq24"]  # Read from environment variable
+BOT_TOKEN = os.environ["BOT_TOKEN"] # Read from environment variable
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Welcome! Use /listcommands to see available commands.")
