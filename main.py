d08172de781a334bdb04aa5eb024fc4684d7fd89
Maii <liebeee.maii@gmail.com>
2025-05-17 17:02:02 +0700

    Update main.py

--- main.py
+++ main.py
@@ -12,27 +12,49 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
 async def listcommands(update: Update, context: ContextTypes.DEFAULT_TYPE):
     message = """
 ---- <Common Commands> ----
+
+LIST COMMANDS:
 /listcommands
+LIST WINNERS:
 /listwinners
+ONLINE MEMBER:
 /onlinemember
+CHECK PROVIDERS STATUS:
 /provider
+CHECK MAIN ACCOUNT BALANCE:
 /balance
+CHECK ALL VALID DOMAIN:
 /domain
+CHECK WHITE LIST:
 /whitelist
+PROMPTS OR COMMANDS:
 /prompts
+HOW TO USE BOT:
 /help
 
+CHECK-MEMBER
+
+BOT-NEWONE
 ---- <Super Commands> ----
+
+Active Bot:
 /onbot
+Suspend Bot:
 /offbot
+Supported Currency:
 /currency
+Providers Winloss Report:
 /list_all_winloss
+Total Winloss Report:
 /list_total_winloss
+Total Deposit Report:
 /list_total_deposit
+Total Withdraw Report:
 /list_total_withdraw
+Total Register Report:
 /list_total_register
 
-Send Request: https://forms.gle/Qz3qfYRkLXrJf9VU6
+Have Any Request? Send it via: https://forms.gle/Qz3qfYRkLXrJf9VU6
 """
     await update.message.reply_text(message)
 
